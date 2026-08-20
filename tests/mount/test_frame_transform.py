705a92c997780df1b88915caa0287cadec342af3
Alex Grant <alex.grant@improbability.engineering>
2026-08-21 05:11:03 +0930

    Strengthen frame transform characterization test

--- tests/mount/test_frame_transform.py
+++ tests/mount/test_frame_transform.py
@@ -21,15 +21,39 @@ def test_frame_transform_reports_missing_astropy(monkeypatch):
     not frame_transform.ASTROPY_AVAILABLE,
     reason="astropy is not installed",
 )
-def test_frame_transform_preserves_existing_round_trip_behavior():
-    time_utc = datetime.datetime(2026, 8, 21, 12, 0, tzinfo=datetime.timezone.utc)
-    ra_rad = 1.2
-    dec_rad = 0.4
+def test_frame_transform_preserves_existing_astropy_behavior():
+    import astropy.units as u
+    from astropy.coordinates import FK5, SkyCoord
+    from astropy.time import Time
 
-    jnow_ra, jnow_dec = frame_transform.icrs_to_jnow(ra_rad, dec_rad, time_utc)
-    round_trip_ra, round_trip_dec = frame_transform.jnow_to_icrs(
-        jnow_ra, jnow_dec, time_utc
+    time_utc = datetime.datetime(2026, 8, 21, 12, 0, tzinfo=datetime.timezone.utc)
+
+    icrs_ra_rad = 1.2
+    icrs_dec_rad = 0.4
+    expected_jnow = SkyCoord(
+        ra=icrs_ra_rad * u.rad,
+        dec=icrs_dec_rad * u.rad,
+        frame="icrs",
+    ).transform_to(FK5(equinox=Time(time_utc)))
+
+    jnow_ra, jnow_dec = frame_transform.icrs_to_jnow(
+        icrs_ra_rad, icrs_dec_rad, time_utc
     )
 
-    assert math.isclose(round_trip_ra, ra_rad, rel_tol=0.0, abs_tol=1e-12)
-    assert math.isclose(round_trip_dec, dec_rad, rel_tol=0.0, abs_tol=1e-12)
+    assert math.isclose(jnow_ra, expected_jnow.ra.rad, rel_tol=0.0, abs_tol=1e-12)
+    assert math.isclose(jnow_dec, expected_jnow.dec.rad, rel_tol=0.0, abs_tol=1e-12)
+
+    fixed_jnow_ra_rad = 1.3
+    fixed_jnow_dec_rad = 0.35
+    expected_icrs = SkyCoord(
+        ra=fixed_jnow_ra_rad * u.rad,
+        dec=fixed_jnow_dec_rad * u.rad,
+        frame=FK5(equinox=Time(time_utc)),
+    ).transform_to("icrs")
+
+    icrs_ra, icrs_dec = frame_transform.jnow_to_icrs(
+        fixed_jnow_ra_rad, fixed_jnow_dec_rad, time_utc
+    )
+
+    assert math.isclose(icrs_ra, expected_icrs.ra.rad, rel_tol=0.0, abs_tol=1e-12)
+    assert math.isclose(icrs_dec, expected_icrs.dec.rad, rel_tol=0.0, abs_tol=1e-12)
